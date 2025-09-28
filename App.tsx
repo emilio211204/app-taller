6629912457cabdfbad7bb7f010999884dea463cb
emilio211204 <joseulcuango32@gmail.com>
2025-09-28 18:57:53 -0500

    Add files via upload
    app

---
+++ App.tsx
@@ -0,0 +1,22 @@
+import 'react-native-gesture-handler';
+import 'react-native-reanimated';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Navegador from './navigation/Navegador';
+
+
+
+export default function App() {
+  return (
+    <Navegador />
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
